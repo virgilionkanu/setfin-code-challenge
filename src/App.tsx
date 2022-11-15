6a9f9962d0e08e1b9f732666d607731045f7808d
virgilionkanu <virgiliocano53@gmail.com>
2022-11-14 23:43:44 -0300

    refactor: configuração inicial do projeto, e estruturação dos diretórios

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-function App() {
+import React from 'react';
+
+export const App: React.FC = () => {
   return (
     <div className="App">
       <h1>SETFIN</h1>
     </div>
   );
-}
-
-export default App;
+};
